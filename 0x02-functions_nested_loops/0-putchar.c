1744756767ae84de679b3be40ae93c084c285dbd
noumoi <holawaleh.software@gmail.com>
2023-10-15 18:57:35 +0100

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,13 +6,13 @@
 **/
 int main(void)
 {
-	char word[9] = "_putchar";
-	int i = 0;
-	while (i < 8)
-	{
-		_putchar(word[i]);
-		i++;
-	}
-	_putchar('\n');
-	return (0);
+char word[9] = "_putchar";
+int i = 0;
+while (i < 8)
+{
+_putchar(word[i]);
+i++;
+}
+_putchar('\n');
+return (0);
 }
