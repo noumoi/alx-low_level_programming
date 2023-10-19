2614333a32d12b857033849e01cb9058d038e308
noumoi <holawaleh.software@gmail.com>
2023-10-19 07:02:13 +0100

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,13 +6,13 @@
 * 0 and prints 0 if n is zero
 * -1 and prints - if n is less than zero
 */
-int print_sign (int n)
+int print_sign(int n)
 {
 if (n > 0)
 {
 _putchar ('+');
 return (1);
-} 
+}
 else if (n == 0)
 {
 _putchar (48);
