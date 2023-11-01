6f6a8376f1ec092cc3268fc634653a9226dc459f
noumoi <holawaleh.software@gmail.com>
2023-11-01 06:36:35 +0100

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,22 +1,45 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
-/**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
- */
-int main(int ac, char *av[])
-{
-    char *s;
 
-    s = argstostr(ac, av);
-    if (s == NULL)
-    {
-        return (1);
-    }
-    printf("%s", s);
-    free(s);
-    return (0);
+/**
+ *argstostr - concatenates all arguments of the program
+ *@ac: integer
+ *@av: character
+ *
+ *Return: pointer
+ */
+
+char *argstostr(int ac, char **av)
+{
+char *c;
+int i, j, size = 0, k = 0;
+
+if (ac == 0 || av == 0)
+return (NULL);
+
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j]; j++)
+{
+size++;
+}
+size++;
+}
+size++;
+
+c = malloc(size);
+
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j]; j++)
+{
+c[k] = av[i][j];
+k++;
+}
+c[k] = '\n';
+k++;
+}
+c[k] = '\0';
+return (c);
 }
