5dedb984ec7aee8a4e8c86e6cc432dd8c0ef756c
noumoi <holawaleh.software@gmail.com>
2023-11-08 23:05:54 +0100

    0-main.c, main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,4 +5,7 @@
 
 void positive_or_negative(int i);
 
-#endif /* MAIN_H */
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int days, int year);
+int convert_days(int month, int day);
+#endif 
